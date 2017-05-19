ca3a0f96666dbe95b8cfd67a437c919dab76a9cc
Mauve3 <mauvemoonman@gmail.com>
2017-05-19 12:38:31 -0700

    init

--- app/conn_nic.py
+++ app/conn_nic.py
@@ -6,8 +6,8 @@ class ConnNic:
   def __init__(self, domain, action):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(60)
-    sock = ssl.wrap_socket(sock, keyfile="./occasio.key", certfile="./cacert.pem", server_side=False, cert_reqs=ssl.CERT_REQUIRED,
-                           ca_certs="./home/bitnami/occasio/python2713/lib/python2.7/site-packages/certifi/cacert.pem")
+    sock = ssl.wrap_socket(sock, keyfile="/home/bitnami/flask-epp/app/occasio.key", certfile="/home/bitnami/flask-epp/app/cacert.pem", server_side=False, cert_reqs=ssl.CERT_REQUIRED,
+                           ca_certs="/home/bitnami/occasio/python2713/lib/python2.7/site-packages/certifi/cacert.pem")
 
     try:
       sock.connect(('ote.nic.io', 700))
