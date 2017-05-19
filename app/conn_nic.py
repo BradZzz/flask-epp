323c97f564265b5109d2e10e0bcd3f7804815b80
Mauve3 <mauvemoonman@gmail.com>
2017-05-19 12:28:47 -0700

    init

--- app/conn_nic.py
+++ app/conn_nic.py
@@ -4,11 +4,6 @@ from commands import Command
 
 class ConnNic:
   def __init__(self, domain, action):
-    self.vars = {
-      domain : domain,
-      action : action,
-    }
-
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(60)
     sock = ssl.wrap_socket(sock, keyfile="occasio.key", certfile="cacert.pem", server_side=False, cert_reqs=ssl.CERT_REQUIRED,
@@ -17,7 +12,8 @@ class ConnNic:
     try:
       sock.connect(('ote.nic.io', 700))
       sock.recv().decode("latin1")
-      self.command = Command(self.vars['domain'], self.vars['action'], sock)
+
+      self.command = Command(domain, action, sock)
       self.command.login()
 
       perform = {
@@ -25,8 +21,8 @@ class ConnNic:
         'backorder' : self.command.backorder,
       }
 
-      if self.vars.action in perform:
-        perform[self.vars.action]
+      if action in perform:
+        perform[action]()
 
     finally:
       sock.close()
