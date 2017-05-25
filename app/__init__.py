9d33e77a4fc89aee127b27b141678be5f3eac737
Mauve3 <mauvemoonman@gmail.com>
2017-05-24 19:43:20 -0700

    fixes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config['DEBUG'] = False
 
 from app import views
--- run.py
+++ run.py
@@ -2,5 +2,5 @@
 from app import app
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug = False)
+  app.run(host='0.0.0.0', debug = True, threaded=True)
 # app.run(debug=True)
